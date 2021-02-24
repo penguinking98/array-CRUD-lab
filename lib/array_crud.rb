ad2676f9d276d2353d64a5a4f271ee318a876500
Ben Kramer <benkramer614@gmail.com>
2021-02-24 11:55:04 -0500

    commit tree 364f9d0fd1d573619b04ca10e1fcf4846e0f407f

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,58 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+    numbers = ["1","2","3","4"]
+    
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element);
+    numbers = ["1","2","3","4"]
+    numbers << "arrays!";
+    
+   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    numbers = ["1","2","3","4"]
+    numbers.unshift "wow";
 end
 
 def remove_element_from_end_of_array(array)
-  
+    numbers = ["1","2","3","arrays!"]
+   numbers.pop 
+   
 end
 
 def remove_element_from_start_of_array(array)
-  
+    numbers = ["wow","2","3","arrays!"]
+   numbers.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    numbers = ["am","2","3","arrays!"]
+    numbers_1 = "am"
+    numbers_2 = "2"
+    numbers_3 = "3"
+    numbers_4 = "arrays!"
+    numbers.first
 end
 
 def retrieve_first_element_from_array(array)
-  
+    numbers = ["wow","2","3","arrays!"]
+    numbers_1 = "wow"
+    numbers_2 = "2"
+    numbers_3 = "3"
+    numbers_4 = "arrays!"
+    numbers.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    numbers = ["wow","2","3","arrays!"]
+    numbers_1 = "wow"
+    numbers_2 = "2"
+    numbers_3 = "3"
+    numbers_4 = "arrays!"
+    numbers.last
 end
